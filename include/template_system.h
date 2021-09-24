066b047068c07cc1b65d8775636559425a44bf61
ravi688 <rp0412204@gmail.com>
2021-09-25 02:30:58 +0530

    Added tuple data structure
    Create a new folder include/tuple [template based]. We can create tuples by just instantiating time as instantiate_tuple_t(int, float). But there is a problem, we can't do that it with pointers because "*" symbol isn't a good character. So we would have to create typedefs for each pointer type.

--- include/template_system.h
+++ include/template_system.h
@@ -6,4 +6,7 @@
 #define template(typename, T) typename##_##T
 
 
+#define template1(typename, T) template(typename, T)
+#define template2(typename, T1, T2) typename##_##T1##T2
+
 #endif/*__HPML_TEMPLATE_SYSTEM_H__*/
---
+++ include/tuple/template_instantiations.h
@@ -0,0 +1,24 @@
+
+#pragma once
+
+
+#include <tuple/template_definitions.h>
+
+typedef char* pchar_t;
+
+instantiate_tuple_t(int, pchar_t); 
+
+instantiate_declaration_tuple_get1(int, pchar_t);
+instantiate_declaration_tuple_get2(int, pchar_t);
+instantiate_declaration_tuple_set1(int, pchar_t);
+instantiate_declaration_tuple_set2(int, pchar_t);
+
+
+#ifdef IMPLEMENTATION
+
+instantiate_implementation_tuple_get1(int, pchar_t); 
+instantiate_implementation_tuple_get2(int, pchar_t);
+instantiate_implementation_tuple_set1(int, pchar_t); 
+instantiate_implementation_tuple_set2(int, pchar_t);
+
+#endif
