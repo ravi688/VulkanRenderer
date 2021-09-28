55831369eae4e9891544222517d7bbb6b06205c6
ravi688 <rp0412204@gmail.com>
2021-09-28 22:10:32 +0530

    Added malloc allocator in debug mode in garbage_collector
    When we will build our project in RELEASE mode then garbage_collector will use tgc_alloc allocator, otherwise in DEBUG mode, it will use the malloc allocator..

--- include/garbage_collector/garbage_collector.h
+++ include/garbage_collector/garbage_collector.h
@@ -6,13 +6,26 @@
 
 extern tgc_t gc;
 
-#define GC_START(stack_address) tgc_start(&gc, (void*)(stack_address))
-#define GC_STOP() tgc_stop(&gc)
-#define GC_RUN() tgc_run(&gc)
-#define GC_PAUSE() tgc_pause(&gc)
-#define GC_RESUME() tgc_resume(&gc)
-#define GC_ALLOC(size) tgc_alloc(&gc, size)
-#define GC_FREE(ptr) tgc_free(&gc, ptr)
+#ifndef DEBUG
+#	define GC_START(stack_address) tgc_start(&gc, (void*)(stack_address))
+#	define GC_STOP() tgc_stop(&gc)
+#	define GC_RUN() tgc_run(&gc)
+#	define GC_PAUSE() tgc_pause(&gc)
+#	define GC_RESUME() tgc_resume(&gc)
+#	define GC_ALLOC(size) tgc_alloc(&gc, size)
+#	define GC_FREE(ptr) tgc_free(&gc, ptr)
+# 	warning "Using GC_ALLOC"
+#else
+#	include <stdlib.h>
+#	define GC_START(stack_address)
+#	define GC_STOP()
+#	define GC_RUN()
+#	define GC_PAUSE()
+#	define GC_RESUME()
+#	define GC_ALLOC(size) malloc(size)
+#	define GC_FREE(ptr) free(ptr)
+#	warning "Using MALLOC"
+#endif
 
 #define GC_NEW(type) (type*)GC_ALLOC(sizeof(type))
 #define GC_NEWV(type, count) (type*)GC_ALLOC(sizeof(type) * (count))
