64deb1592a1d7874db373bcd327d3f694abb9a95
ravi688 <rp0412204@gmail.com>
2021-12-22 01:55:32 +0530

    Added vulkan_buffer.h
    Header: include/renderer/internal/vulkan/vulkan_buffer.h
    Source: source/renderer/vulkan/vulkan_buffer.c

---
+++ include/renderer/internal/vulkan/vulkan_buffer.h
@@ -0,0 +1,31 @@
+
+#pragma once
+
+#include <vulkan/vulkan_wrapper.h>
+
+typedef struct renderer_t renderer_t;
+
+typedef struct vulkan_buffer_create_info_t
+{
+	void* data;
+	uint32_t stride;
+	uint32_t count;
+	VkBufferUsageFlags usage_flags;
+	VkSharingMode sharing_mode;
+	VkMemoryPropertyFlags memory_property_flags;
+} vulkan_buffer_create_info_t;
+
+typedef struct vulkan_buffer_t
+{
+	VkBuffer handle;
+	VkDeviceMemory memory;
+	uint32_t stride;
+	uint32_t count;
+} vulkan_buffer_t;
+
+
+vulkan_buffer_t* vulkan_buffer_new();
+vulkan_buffer_t* vulkan_buffer_create(renderer_t* renderer, vulkan_buffer_create_info_t* create_info);
+void vulkan_buffer_create_no_alloc(renderer_t* renderer, vulkan_buffer_create_info_t* create_info, vulkan_buffer_t* buffer);
+void vulkan_buffer_destroy(vulkan_buffer_t* buffer, renderer_t* renderer);
+void vulkan_buffer_release_resources(vulkan_buffer_t* buffer);
