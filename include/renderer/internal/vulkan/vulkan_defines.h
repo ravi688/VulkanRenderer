eaf6397d0f7d568515dd7a931093ae5033014337
ravi688 <rp0412204@gmail.com>
2022-04-30 16:14:57 +0530

    Fixed best practice warning for Mali GPUs, Command Buffer usage should be ONE_TIME_SUBMIT by default

--- include/renderer/internal/vulkan/vulkan_defines.h
+++ include/renderer/internal/vulkan/vulkan_defines.h
@@ -3,12 +3,7 @@
 
 #ifdef DEBUG
 #	include <renderer/internal/vulkan/vulkan_result.h>
-#	define vkCall(call)\
-{\
-	VkResult result =  call;\
-	vulkan_result_assert_success(result);\
-}
-
+#	define vkCall(result) vulkan_result_assert_success(result)
 #else
 #	define vkCall(call) call
 #endif
