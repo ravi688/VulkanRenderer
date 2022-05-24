24acb92bb501913022c4f6c8693945c43e21843e
ravi688 <rp0412204@gmail.com>
2022-05-24 21:40:13 +0530

    [REFACTORED] Refactored vulkan graphics pipeline
    Now vulkan graphics pipeline doesn't compose the pipeline layout in it itself but instead it is independent of pipeline layout.
    Now vulkan graphics pipeline creation is generic.

--- include/renderer/internal/vulkan/vulkan_graphics_pipeline.h
+++ include/renderer/internal/vulkan/vulkan_graphics_pipeline.h
@@ -5,35 +5,51 @@
 #include <renderer/defines.h>
 #include <renderer/internal/vulkan/vulkan_types.h>
 
-typedef struct vulkan_renderer_t vulkan_renderer_t;
-typedef struct vulkan_shader_t vulkan_shader_t;
 typedef struct vulkan_pipeline_layout_t vulkan_pipeline_layout_t;
-
+typedef struct GraphicsPipeline GraphicsPipeline;
+typedef GraphicsPipeline GraphicsPipelineSettings;
+typedef struct vulkan_shader_module_t vulkan_shader_module_t;
+typedef struct vulkan_render_pass_t vulkan_render_pass_t;
 
 typedef struct vulkan_graphics_pipeline_create_info_t
 {
-	vulkan_shader_t* shader;
-	uint32_t vertex_info_count;
-	vulkan_vertex_info_t* vertex_infos;
-	uint32_t push_constant_range_count;
-	VkPushConstantRange* push_constant_ranges;
-	VkDescriptorSetLayout vk_set_layout;
+	/* pipeline layout for this pipeline */
+	vulkan_pipeline_layout_t* layout;
+	
+	/* fixed function settings for this pipeline */
+	GraphicsPipelineSettings* settings;
+	
+	/* list of shader modues for this pipeline */
+	vulkan_shader_module_t* shader_modules;
+	u32 shader_module_count;
+
+	/* list of vertex binding descriptions supported by this pipeline  */
+	vulkan_vertex_info_t* vertex_attribute_bindings;
+	u32 vertex_attribute_binding_count;
+
+	/* render pass for this pipeline */
+	vulkan_render_pass_t* render_pass;
+	/* subpass index for this pipeline */
+	u32 subpass_index;
+	
 } vulkan_graphics_pipeline_create_info_t;
 
 typedef struct vulkan_graphics_pipeline_t
 {
-	VkPipeline pipeline;
-	vulkan_pipeline_layout_t* pipeline_layout;
+	vulkan_renderer_t* renderer;
+	VkPipeline vo_pipeline;
 } vulkan_graphics_pipeline_t;
 
 BEGIN_CPP_COMPATIBLE
 
+/* constructors & destructors */
 RENDERER_API vulkan_graphics_pipeline_t* vulkan_graphics_pipeline_new();
 RENDERER_API vulkan_graphics_pipeline_t* vulkan_graphics_pipeline_create(vulkan_renderer_t* renderer, vulkan_graphics_pipeline_create_info_t* create_info);
-RENDERER_API void vulkan_graphics_pipeline_create_no_alloc(vulkan_renderer_t* renderer, vulkan_graphics_pipeline_create_info_t* create_info, vulkan_graphics_pipeline_t* pipeline);
-RENDERER_API void vulkan_graphics_pipeline_destroy(vulkan_graphics_pipeline_t* pipeline, vulkan_renderer_t* renderer);
+RENDERER_API void vulkan_graphics_pipeline_create_no_alloc(vulkan_renderer_t* renderer, vulkan_graphics_pipeline_create_info_t* create_info, vulkan_graphics_pipeline_t OUT pipeline);
+RENDERER_API void vulkan_graphics_pipeline_destroy(vulkan_graphics_pipeline_t* pipeline);
 RENDERER_API void vulkan_graphics_pipeline_release_resources(vulkan_graphics_pipeline_t* pipeline);
-RENDERER_API void vulkan_graphics_pipeline_bind(vulkan_graphics_pipeline_t* pipeline, vulkan_renderer_t* renderer);
 
+/* logic functions */
+RENDERER_API void vulkan_graphics_pipeline_bind(vulkan_graphics_pipeline_t* pipeline);
 
 END_CPP_COMPATIBLE
