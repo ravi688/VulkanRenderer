9e790e1b0856564909393af0fc9a17c9d1b73fa3
ravi688 <rp0412204@gmail.com>
2022-11-09 02:12:31 +0530

    Modified assert.h debug.h and defines.h to reference common's headers

--- toolchain/shader_compiler/include/shader_compiler/defines.h
+++ toolchain/shader_compiler/include/shader_compiler/defines.h
@@ -1,20 +1,6 @@
-
 #pragma once
 
-#include <stdint.h>
-#include <stdbool.h>
-
-typedef uint16_t u16; 
-typedef int16_t s16;
-
-typedef uint32_t u32;
-typedef int32_t s32; 
-
-typedef uint64_t u64; 
-typedef int64_t s64; 
-
-typedef uint8_t u8; 
-typedef int8_t s8; 
+#include <common/defines.h>
 
 #ifdef SC_STATIC_LIBRARY
 #	define SC_API
