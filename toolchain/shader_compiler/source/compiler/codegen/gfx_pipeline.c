663dae34bb8e71987ff438bd76b656154669ce7e
ravi688 <rp0412204@gmail.com>
2022-11-24 00:26:09 +0530

    [Bugfix] Fixed the segmentfault when linking the gfx pipeline

--- toolchain/shader_compiler/source/compiler/codegen/gfx_pipeline.c
+++ toolchain/shader_compiler/source/compiler/codegen/gfx_pipeline.c
@@ -567,6 +567,9 @@ static void link(UserData* data)
 	// copy (push) to main output buffer
 	buf_pushv(data->mainOutput, defaultValue, size);
 
+	/* update the pipeline object pointer as the buffer has been resized and the object lies in that buffer */
+	pipeline = buf_get_ptr_at(data->mainOutput, data->mainBaseOffset);
+
 	// set the default values
 	defaultValue = (void*)pipeline->viewport.pViewports;
 	size = sizeof(VkViewport) * pipeline->viewport.viewportCount;
