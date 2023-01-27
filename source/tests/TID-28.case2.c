61bd0ab22d441bb491718b2d4656e8e628c82f9c
ravi688 <rp0412204@gmail.com>
2023-01-28 00:24:57 +0530

    Updated TID_28_CASE_2

--- source/tests/TID-28.case2.c
+++ source/tests/TID-28.case2.c
@@ -15,13 +15,15 @@ TEST_DATA(TID_28_CASE_2)
 
 	/* cameras */
 	camera_t* camera;
+	camera_t* offscreenCamera;
 
 	/* lights */
-	light_t* light;
+	light_t* pointLight;
 
 	/* materials */
 	material_t* material;
-	material_t* unlitMaterial;
+	material_t* shadowMapMaterial;
+	material_t* depthReflectionMaterial;
 
 	/* meshes */
 	mesh_t* mesh;
@@ -32,6 +34,9 @@ TEST_DATA(TID_28_CASE_2)
 	render_object_t* renderObject;
 	render_object_t* wallObject;
 	render_object_t* cubeObject;
+
+	/* textures */
+	render_texture_t* shadowMap;
 };
 
 SETUP_TEST(TID_28_CASE_2);
@@ -44,28 +49,51 @@ TEST_ON_INITIALIZE(TID_28_CASE_2)
 
 	this->camera = camera_system_getH(camera_system,
 		camera_system_create_camera(camera_system, CAMERA_PROJECTION_TYPE_PERSPECTIVE));
-	camera_set_clear(this->camera, COLOR_BLACK, 1.0f);
+	camera_set_clear(this->camera, COLOR_WHITE, 1.0f);
 	camera_set_active(this->camera, true);
+	
+	this->offscreenCamera = camera_system_getH(camera_system,
+		camera_system_create_camera(camera_system, CAMERA_PROJECTION_TYPE_PERSPECTIVE));
+	camera_set_clear(this->offscreenCamera, COLOR_RED, 1.0f);
+	camera_set_position(this->offscreenCamera, vec3_zero());
+	camera_set_rotation(this->offscreenCamera, vec3(0, 0, 0));
+	camera_set_field_of_view(this->offscreenCamera, 90 DEG);
+	camera_set_active(this->offscreenCamera, false);
 
-	this->scene = render_scene_create_from_mask(renderer, BIT64(RENDER_QUEUE_TYPE_GEOMETRY));
-	this->light = light_create(renderer, LIGHT_TYPE_SPOT);
-	light_set_rotation(this->light, vec3(0, 0, 0));
-	light_set_position(this->light, vec3(-0.5, 0, 0));
-	light_set_spot_angle(this->light, 90 DEG);
+	this->scene = render_scene_create_from_mask(renderer, BIT64(RENDER_QUEUE_TYPE_GEOMETRY) | BIT64(RENDER_QUEUE_TYPE_QUEUE0));
+	this->pointLight = light_create(renderer, LIGHT_TYPE_POINT);
+	light_set_position(this->pointLight, vec3_zero());
+	light_set_intensity(this->pointLight, 0.5f);
 
+	this->shadowMapMaterial = material_library_getH(mlib, 
+							material_library_create_materialH(mlib, 
+							shader_library_load_shader(slib, 
+								"shaders/presets/shadowmap.sb"), "ShadowMapMaterial"));
 	this->material = material_library_getH(mlib, 
 							material_library_create_materialH(mlib, 
 							shader_library_load_shader(slib, 
-								"shaders/presets/spot_light.sb"), "SpotLightMaterial"));
-
-	material_set_vec4(this->material, "parameters.color", vec4(1, 1, 1, 1));
-
-	this->unlitMaterial = material_library_getH(mlib, 
+								"shaders/presets/point_light.sb"), "PointLightMaterial"));
+	this->depthReflectionMaterial = material_library_getH(mlib, 
 							material_library_create_materialH(mlib, 
 							shader_library_load_shader(slib, 
-								"shaders/presets/unlit.color.sb"), "UnlitMaterial"));
+								"shaders/presets/reflection.depth.point.sb"), "DepthRefectionMaterial"));
 
-	material_set_vec4(this->unlitMaterial, "parameters.color", vec4(1, 1, 1, 1));
+	material_set_vec4(this->material, "parameters.color", vec4(1, 1, 1, 1));
+	material_set_vec4(this->depthReflectionMaterial, "parameters.color", vec4(1, 1, 1, 1));
+	material_set_float(this->depthReflectionMaterial, "parameters.reflectance", 1.0f);
+
+	render_texture_create_info_t create_info = 
+	{
+		.width = renderer_get_window(renderer)->width,
+		.height = renderer_get_window(renderer)->height,
+		.depth = 1,
+		.channel_count = 4,
+		.type = RENDER_TEXTURE_TYPE_DEPTH_CUBE
+	};	
+	this->shadowMap = render_texture_create(renderer, &create_info);
+	camera_set_render_target(this->offscreenCamera, CAMERA_RENDER_TARGET_TYPE_DEPTH, this->shadowMap);
+	material_set_texture(this->depthReflectionMaterial, "reflectionMap", this->shadowMap);
+	material_set_texture(this->material, "shadowMap", this->shadowMap);
 
 	AUTO sphereMeshData = mesh3d_load("models/Monkey.obj");
 	mesh3d_make_centroid_origin(sphereMeshData);
@@ -74,14 +102,15 @@ TEST_ON_INITIALIZE(TID_28_CASE_2)
 	this->renderObject = render_scene_getH(this->scene, render_scene_create_object(this->scene, RENDER_OBJECT_TYPE_MESH, RENDER_QUEUE_TYPE_GEOMETRY));
 	render_object_set_material(this->renderObject, this->material);
 	render_object_attach(this->renderObject, this->mesh);
-	render_object_set_transform(this->renderObject, mat4_mul(2, mat4_rotation(0, -90 DEG, 0), mat4_scale(0.3f, 0.3f, 0.3f)));
+	render_object_set_transform(this->renderObject, mat4_mul(2, mat4_rotation(0, -90 DEG, 0), mat4_scale(0.5f, 0.5f, 0.5f)));
 
 	AUTO cubeMeshData = mesh3d_cube(1);
 	this->cubeMesh = mesh_create(renderer, cubeMeshData);
 	this->cubeObject = render_scene_getH(this->scene, render_scene_create_object(this->scene, RENDER_OBJECT_TYPE_MESH, RENDER_QUEUE_TYPE_GEOMETRY));
-	render_object_set_material(this->cubeObject, this->unlitMaterial);
+	render_object_set_material(this->cubeObject, this->material);
 	render_object_attach(this->cubeObject, this->cubeMesh);
 	render_object_set_transform(this->cubeObject, mat4_mul(2, mat4_translation(-0.5f, 0, 0.5f), mat4_scale(0.1f, 0.1f, 0.1f)));
+
 	mesh3d_transform_set(cubeMeshData, mat4_scale(-1, -1, -1));
 	this->wallMesh = mesh_create(renderer, cubeMeshData);
 	this->wallObject = render_scene_getH(this->scene, render_scene_create_object(this->scene, RENDER_OBJECT_TYPE_MESH, RENDER_QUEUE_TYPE_GEOMETRY));
@@ -99,32 +128,56 @@ TEST_ON_TERMINATE(TID_28_CASE_2)
 	mesh_release_resources(this->wallMesh);
 	mesh_destroy(this->cubeMesh);
 	mesh_release_resources(this->cubeMesh);
-	light_destroy(this->light);
-	light_release_resources(this->light);
+	light_destroy(this->pointLight);
+	light_release_resources(this->pointLight);
+
+	render_texture_destroy(this->shadowMap);
+	render_texture_release_resources(this->shadowMap);
 
 	render_scene_destroy(this->scene);
 	render_scene_release_resources(this->scene);
 }
 
 
+static bool swap = false;
 static float angle = 0;
 
 TEST_ON_UPDATE(TID_28_CASE_2)
 {
+	if(kbhit())
+	{
+		getch();
+		swap = !swap;
+		if(swap)
+			camera_set_render_target(this->offscreenCamera, CAMERA_RENDER_TARGET_TYPE_DEPTH, CAMERA_RENDER_TARGET_SCREEN);
+		else 
+			camera_set_render_target(this->offscreenCamera, CAMERA_RENDER_TARGET_TYPE_DEPTH, this->shadowMap);
+	}
+
 	angle += deltaTime;
 	float _angle = (0.5f * sin(angle) + 0.5f) * 180.0f - 180.0f;
-	vec3_t pos = vec3(sin(_angle DEG), 0, cos(_angle DEG));
-	light_set_position(this->light, pos);
-	light_set_rotation(this->light, vec3(0, (90 + _angle) DEG, 0));
-	AUTO color = vec3(pos.x * 0.5f + 0.5f, pos.y * 0.5f + 0.5f, pos.z * 0.5f + 0.5f);
-	light_set_color(this->light, color);
-	material_set_vec4(this->unlitMaterial, "parameters.color", vec4(color.x, color.y, color.z, 1));
+	vec3_t pos = vec3(0.8f * sin(_angle DEG), 0, 0.8f * cos(_angle DEG));
+	vulkan_camera_set_position_cube(this->offscreenCamera, pos);
+	light_set_position(this->pointLight, pos);
+	light_set_color(this->pointLight, vec3(pos.x * 0.5f + 0.5f, pos.y * 0.5f + 0.5f, pos.z * 0.5f + 0.5f));
 	render_object_set_transform(this->cubeObject, mat4_mul(2, mat4_translation(pos.x, pos.y, pos.z),
-		mat4_scale(0.05f, 0.05f, 0.05f)));
+		mat4_scale(0.1f, 0.1f, 0.1f)));
 }
 
 TEST_ON_RENDER(TID_28_CASE_2)
 {
+	render_object_set_material(this->renderObject, this->shadowMapMaterial);
+	render_object_set_material(this->wallObject, this->shadowMapMaterial);
+	render_object_set_material(this->cubeObject, this->shadowMapMaterial);
+	camera_set_active(this->offscreenCamera, true);
+	camera_set_active(this->camera, false);
+	render_scene_render(this->scene, RENDER_SCENE_ALL_QUEUES, RENDER_SCENE_CLEAR);
+
+	render_object_set_material(this->renderObject, this->material);
+	render_object_set_material(this->wallObject, this->material);
+	render_object_set_material(this->cubeObject, this->material);
+	camera_set_active(this->offscreenCamera, false);
+	camera_set_active(this->camera, true);
 	render_scene_render(this->scene, RENDER_SCENE_ALL_QUEUES, RENDER_SCENE_CLEAR);
 }
 
