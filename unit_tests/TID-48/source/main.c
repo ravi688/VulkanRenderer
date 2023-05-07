bd6abf4dfb971c43612fde14c786d6a0de7435e4
ravi688 <rp0412204@gmail.com>
2023-05-07 15:10:12 +0530

    Added Optimized Render Path Algorithm

--- unit_tests/TID-48/source/main.c
+++ unit_tests/TID-48/source/main.c
@@ -50,6 +50,8 @@ typedef struct RenderPassGraphNode
 {
 	RenderPassGraphSlotHandle slotHandle;
 	RenderPassGraphNodeHandleList nextPassNodeHandles;
+	u32 prevPassCount;
+	u32 depth;
 } RenderPassGraphNode;
 
 typedef BUFFER /* RenderPassGraphSlot */ RenderPassGraphSlotList;
@@ -61,7 +63,7 @@ typedef struct RenderPassGraph
 	RenderPassGraphSlotList slots;
 	RenderPassGraphNodeList nodes;
 	RenderPassGraphSlotHandle prevPassNodeHandle;
-	RenderPassGraphNodePtrStack nodePtrStack;
+	RenderPassGraphNodeHandleList optimizedRenderPath;
 } RenderPassGraph;
 
 static RenderPassGraph* RenderPassGraphCreate()
@@ -70,7 +72,7 @@ static RenderPassGraph* RenderPassGraphCreate()
 	graph->slots = buf_new(RenderPassGraphSlot);
 	graph->nodes = buf_new(RenderPassGraphNode);
 	graph->prevPassNodeHandle = RENDER_PASS_GRAPH_NODE_HANDLE_INVALID;
-	graph->nodePtrStack = buf_new(RenderPassGraphNode*);
+	graph->optimizedRenderPath = buf_new(RenderPassGraphNodeHandle);
 	puts("Render Pass Library Created");
 	return graph;
 }
@@ -100,20 +102,7 @@ static bool createInfoComparer(void* value, void* cursor)
 	return createInfo->hash == slot->createInfo.hash;
 }
 
-static void dumpNodePtrStack(RenderPassGraph* graph)
-{
-	printf("Stack: ");
-	u32 count = buf_get_element_count(&graph->nodePtrStack);
-	for(u32 i = 0; i < count; i++)
-	{
-		RenderPassGraphNode* node;
-		buf_get_at(&graph->nodePtrStack, i, &node);
-		printf("%lu ", node->slotHandle + 1);
-	}
-	puts("");
-}
-
-static bool IsCycle(RenderPassGraph* graph, RenderPassGraphNode* node, RenderPassGraphNode* prevNode)
+static bool isCycle(RenderPassGraph* graph, RenderPassGraphNode* node, RenderPassGraphNode* prevNode)
 {
 	if(node == prevNode)
 		return true;
@@ -123,7 +112,7 @@ static bool IsCycle(RenderPassGraph* graph, RenderPassGraphNode* node, RenderPas
 		RenderPassGraphNodeHandle nextHandle;
 		buf_get_at(&node->nextPassNodeHandles, i, &nextHandle);
 		AUTO nextNode = buf_get_ptr_at_typeof(&graph->nodes, RenderPassGraphNode, nextHandle);
-		if(IsCycle(graph, nextNode, prevNode))
+		if(isCycle(graph, nextNode, prevNode))
 			return true;
 	}
 	return false;
@@ -143,9 +132,8 @@ static bool nodeComparer(void* value, void* cursor)
 	if(node->slotHandle == data->slotHandle)
 	{
 		RenderPassGraph* graph = data->graph;
-		_assert(buf_get_element_count(&graph->nodePtrStack) == 0);
 		return (graph->prevPassNodeHandle == RENDER_PASS_GRAPH_NODE_HANDLE_INVALID) ||
-				!IsCycle(graph, node, buf_get_ptr_at_typeof(&graph->nodes, RenderPassGraphNode, graph->prevPassNodeHandle));
+				!isCycle(graph, node, buf_get_ptr_at_typeof(&graph->nodes, RenderPassGraphNode, graph->prevPassNodeHandle));
 	}
 
 	return false;
@@ -154,6 +142,11 @@ static bool nodeComparer(void* value, void* cursor)
 static bool renderPassGraphNodeHandleComparer(void* value, void* cursor)
 {
 	return DREF_TO(RenderPassGraphNodeHandle, value) == DREF_TO(RenderPassGraphNodeHandle, cursor);
+}
+
+static u32 max(u32 v1, u32 v2)
+{
+	return (v1 > v2) ? v1 : v2;
 }
 
 static RenderPassGraphSlotHandle RenderPassGraphCreatePass(RenderPassGraph* graph, const RenderPassCreateInfo* createInfo)
@@ -184,6 +177,8 @@ static RenderPassGraphSlotHandle RenderPassGraphCreatePass(RenderPassGraph* grap
 		node = CAST_TO(RenderPassGraphNode*, buf_peek_ptr(&graph->nodes));
 		node->slotHandle = slotIndex;
 		node->nextPassNodeHandles = buf_new(RenderPassGraphNodeHandle);
+		node->prevPassCount = 0;
+		node->depth = 0;
 		nodeIndex = buf_get_element_count(&graph->nodes) - 1;
 	}
 	else
@@ -193,14 +188,18 @@ static RenderPassGraphSlotHandle RenderPassGraphCreatePass(RenderPassGraph* grap
 	{
 		AUTO prevNode = buf_get_ptr_at_typeof(&graph->nodes, RenderPassGraphNode, graph->prevPassNodeHandle);
 		if(buf_find_index_of(&prevNode->nextPassNodeHandles, (void*)(&nodeIndex), renderPassGraphNodeHandleComparer) == BUF_INVALID_INDEX)
+		{
 			buf_push(&prevNode->nextPassNodeHandles, &nodeIndex);
+			node->prevPassCount++;
+		}
+		node->depth = max(prevNode->depth + 1, node->depth);
 	}
  
 	graph->prevPassNodeHandle = nodeIndex;
 	return (RenderPassGraphSlotHandle)slotIndex;
 }
 
-static void RenderPassGraphDump(RenderPassGraph* graph)
+static void RenderPassGraphDump(RenderPassGraph* graph, bool isPrintable)
 {
 	puts("digraph\n{");
 	u32 nodeCount = buf_get_element_count(&graph->nodes);
@@ -208,21 +207,96 @@ static void RenderPassGraphDump(RenderPassGraph* graph)
 	{
 		AUTO node = buf_get_ptr_at_typeof(&graph->nodes, RenderPassGraphNode, i);
 		u32 nextPassCount = buf_get_element_count(&node->nextPassNodeHandles);
-		printf("\t%lu(%lu) -> { ", i + 1, node->slotHandle + 1);
+		if(isPrintable)
+			printf("\t%lu -> { ", i + 1);
+		else
+			printf("\t%lu(%lu, %lu) -> { ", i + 1, node->slotHandle + 1, node->depth);
 		for(u32 j = 0; j < nextPassCount; j++)
 		{
 			RenderPassGraphNodeHandle nodeHandle = *buf_get_ptr_at_typeof(&node->nextPassNodeHandles, RenderPassGraphNodeHandle, j);
 			RenderPassGraphSlotHandle slotHandle = buf_get_ptr_at_typeof(&graph->nodes, RenderPassGraphNode, nodeHandle)->slotHandle;
-			printf("%lu(%lu) ", nodeHandle + 1, slotHandle + 1);
+			if(isPrintable)
+				printf("%lu ", nodeHandle + 1);
+			else
+				printf("%lu(%lu) ", nodeHandle + 1, slotHandle + 1);
 		}
 		puts("}");
 	}
 	puts("}");
 }
 
-static void RunRenderPaths(RenderPassGraph* graph)
+static bool isAllDependenciesResolved(RenderPassGraph* graph, RenderPassGraphNode* node, RenderPassGraphNodeHandle nodeHandle)
+{
+	u32 dependencyCount = node->prevPassCount;
+	u32 count = buf_get_element_count(&graph->optimizedRenderPath);
+	for(u32 i = 0; i < count; i++)
+	{
+		RenderPassGraphNodeHandle _nodeHandle;
+		buf_get_at(&graph->optimizedRenderPath, i, &_nodeHandle);
+
+		/* if this node is a dependency of the node for which this function has been called then decrement the dependency count */
+		if(buf_find_index_of(&buf_get_ptr_at_typeof(&graph->nodes, RenderPassGraphNode, _nodeHandle)->nextPassNodeHandles, 
+							(void*)(&nodeHandle), buf_u32_comparer) != BUF_INVALID_INDEX)
+			dependencyCount--;
+	}
+	return dependencyCount == 0;
+}
+
+static void sortStagingList(RenderPassGraph* graph, BUFFER* stagingList)
 {
 
+}
+
+static void BuildOptimizedRenderPath(RenderPassGraph* graph)
+{
+	u32 count = buf_get_element_count(&graph->nodes);
+	u32 nodeCount = count;
+	BUFFER stagingList = buf_new(RenderPassGraphNodeHandle);
+	u32 depth = 0;
+	while(count > 0)
+	{
+		/* collect all the nodes (at the same depth), whose dependencies have been resolved, into the staging list */
+		for(u32 i = 0; i < nodeCount; i++)
+		{
+			AUTO node = buf_get_ptr_at_typeof(&graph->nodes, RenderPassGraphNode, i);
+			if((node->depth <= depth) 
+				&& (buf_find_index_of(&graph->optimizedRenderPath, (void*)(&i), buf_u32_comparer) == BUF_INVALID_INDEX) 
+				&& isAllDependenciesResolved(graph, node, i))
+				buf_push(&stagingList, &i);
+		}
+
+		/* now sort nodes in the staging list in the order of decreasing number of child nodes (nextPassHandles) */
+		sortStagingList(graph, &stagingList);
+			
+		/* commit them into optimized render path buffer */
+		u32 stageCount = buf_get_element_count(&stagingList);
+		for(u32 i = 0; i < stageCount; i++)
+			buf_push(&graph->optimizedRenderPath, buf_get_ptr_at(&stagingList, i));
+			
+		/* clear the staging list for the next iteration */
+		buf_clear(&stagingList, NULL);
+		
+		/* decrement the count by the number of nodes which were in the staging buffer (as they are committed) */
+		_assert(count >= stageCount);
+		count -= stageCount;
+
+		depth++;
+	}
+	buf_free(&stagingList);
+}
+
+static void RunOptimizedRenderPath(RenderPassGraph* graph)
+{
+	printf("Optimized Render Path: ");
+	u32 count = buf_get_element_count(&graph->optimizedRenderPath);
+	for(u32 i = 0; i < count; i++)
+	{
+		RenderPassGraphNodeHandle nodeHandle;
+		buf_get_at(&graph->optimizedRenderPath, i, &nodeHandle);
+		AUTO node = buf_get_ptr_at_typeof(&graph->nodes, RenderPassGraphNode, nodeHandle);
+		printf("%lu(%lu) ", nodeHandle + 1, node->slotHandle + 1);
+	}
+	puts("");
 }
 
 /* --- Context --- */
@@ -382,7 +456,6 @@ static Shader* LoadShader6(Context* context)
 		{ .hash = 9 },
 		{ .hash = 4 },
 		{ .hash = 4 },
-		{ .hash = 4 },
 		{ .hash = 6 },
 		{ .hash = 7 }
 	};
@@ -405,8 +478,10 @@ int main()
 	Shader* shader4 = LoadShader4(context);
 	Shader* shader5 = LoadShader5(context);
 	Shader* shader6 = LoadShader6(context);
-	RenderPassGraphDump(context->graph);
-	RunRenderPaths(context->graph);
+	RenderPassGraphDump(context->graph, false);
+	RenderPassGraphDump(context->graph, true);
+	BuildOptimizedRenderPath(context->graph);
+	RunOptimizedRenderPath(context->graph);
 	ShaderDestroy(shader1);
 	ShaderDestroy(shader2);
 	ShaderDestroy(shader3);
