5668935b29b78d7239c1b3ac11b7e0940b08261c
ravi688 <rp0412204@gmail.com>
2024-04-17 01:02:44 +0530

    Added switch to windows support in the gitlab ci

---
+++ switch-to-windows.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+WINDOWS_ENTRY=`grep menuentry /boot/grub/grub.cfg  | grep --line-number Windows`
+MENU_NUMBER=$(( `echo $WINDOWS_ENTRY | sed -e "s/:.*//"` - 1 ))
+sudo grub-reboot 'Windows Boot Manager (on /dev/nvme0n1p1)'
+sudo reboot
