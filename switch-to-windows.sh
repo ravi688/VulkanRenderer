321fe44c7b807bc5e5a9bb72054c4b2856f7d332
ravi688 <rp0412204@gmail.com>
2024-04-17 01:21:10 +0530

    Update gitlab-ci.yml and switch-to-windows.sh

--- switch-to-windows.sh
+++ switch-to-windows.sh
@@ -2,4 +2,4 @@
 WINDOWS_ENTRY=`grep menuentry /boot/grub/grub.cfg  | grep --line-number Windows`
 MENU_NUMBER=$(( `echo $WINDOWS_ENTRY | sed -e "s/:.*//"` - 1 ))
 sudo grub-reboot 'Windows Boot Manager (on /dev/nvme0n1p1)'
-sudo reboot
+sleep 15; sudo reboot
