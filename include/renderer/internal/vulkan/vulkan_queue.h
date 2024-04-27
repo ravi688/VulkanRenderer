b9d6fbd661bf96f2cb15bd126ff076d3be3fdb15
ravi688 <rp0412204@gmail.com>
2024-04-27 20:09:31 +0530

    Followup to previous commit, add vulkan_queue.h also

--- include/renderer/internal/vulkan/vulkan_queue.h
+++ include/renderer/internal/vulkan/vulkan_queue.h
@@ -33,8 +33,8 @@
 
 BEGIN_CPP_COMPATIBLE
 
-RENDERER_API void vulkan_queue_present(VkQueue queue, VkSwapchainKHR swapchain, u32 image_index, VkSemaphore wait);
-RENDERER_API void vulkan_queue_presentv(VkQueue queue, u32 swapchain_count, VkSwapchainKHR* swapchains, u32* image_indices, VkSemaphore wait);
+RENDERER_API bool vulkan_queue_present(VkQueue queue, VkSwapchainKHR swapchain, u32 image_index, VkSemaphore wait);
+RENDERER_API bool vulkan_queue_presentv(VkQueue queue, u32 swapchain_count, VkSwapchainKHR* swapchains, u32* image_indices, VkSemaphore wait);
 RENDERER_API void vulkan_queue_submit(VkQueue queue, VkCommandBuffer buffer, VkSemaphore wait, VkPipelineStageFlags wait_dst_stage, VkSemaphore signal, VkFence fence);
 RENDERER_API void vulkan_queue_submitv(VkQueue queue, u32 count, VkCommandBuffer* buffers, VkSemaphore wait, VkPipelineStageFlags wait_dst, VkSemaphore signal, VkFence fence);
 static inline void vulkan_queue_wait_idle(VkQueue queue) { vkCall(vkQueueWaitIdle(queue)); } 
