1de92f924fcc37da6d30fd28a2996a628a3dd0d2
ravi688 <rp0412204@gmail.com>
2024-05-11 00:09:54 +0530

    [Notice Update] Updated notice in the beginning of each file

--- include/renderer/object.h
+++ include/renderer/object.h
@@ -5,7 +5,7 @@
 	such as toolchains written by the same author) is a software to render 
 	2D & 3D geometries by writing C/C++ code and shaders.
 
-	File: vulkan_object.h is a part of VulkanRenderer
+	File: object.h is a part of VulkanRenderer
 
 	Copyright (C) 2021 - 2024  Author: Ravi Prakash Singh
 
