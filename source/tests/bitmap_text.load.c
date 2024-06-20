4864e94a236249a64ce01caa6e514be7da887dd9
ravi688 <rp0412204@gmail.com>
2024-06-20 13:11:11 +0530

    [Notice Update] Updated notice in each .h, .c and .v3dshader files

--- source/tests/bitmap_text.load.c
+++ source/tests/bitmap_text.load.c
@@ -5,7 +5,7 @@
 	such as toolchains written by the same author) is a software to render 
 	2D & 3D geometries by writing C/C++ code and shaders.
 
-	File: bitmap_text.c is a part of VulkanRenderer
+	File: bitmap_text.load.c is a part of VulkanRenderer
 
 	Copyright (C) 2021 - 2024  Author: Ravi Prakash Singh
 
