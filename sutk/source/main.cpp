e71e69adb2b0a708d9a0e29b662de7d30f98cedf
ravi688 <rp0412204@gmail.com>
2024-07-03 18:48:01 +0530

    [SUTK] Use cpp binding of SGE

--- sutk/source/main.cpp
+++ sutk/source/main.cpp
@@ -1,48 +1,20 @@
 
 #include <sutk/debug.h>
-#include <sutk/assert.h>
 
-#define SGE_INCLUDE_CORE
-#include <sge/sge.h>
+#include <sge-cpp/sge.hpp>
 
 int main(int argc, char** argv)
 {
 	debug_log_info("SUTK Repository!");
 
-	alloc_init(&argv);
+	SGE::Driver driver(SGE::GPUType::Discrete, 800, 800, argv[0], false, true);
 
-	memory_allocator_create_info_t create_info = { };
-	memory_allocator_t* allocator = memory_allocator_create(&create_info);
-
-	renderer_create_info_t driver_create_info = { };
-	driver_create_info.gpu_type = SGE_GPU_TYPE_DISCRETE;
-	driver_create_info.width = 800;
-	driver_create_info.height = 800;
-	driver_create_info.title = argv[0];
-	driver_create_info.full_screen = false;
-	driver_create_info.resizable = true;
-	
-	AUTO driver = renderer_init(allocator, &driver_create_info);
-
-	while(renderer_is_running(driver))
+	while(driver.isRunning())
 	{
-		/* record command buffers for this frame */
-		renderer_begin_frame(driver);
+		driver.beginFrame();
+		driver.endFrame();
 
-		renderer_end_frame(driver);
-
-		/* dispatch command buffers for this frame for execution 
-		 * NOTE: if frame pipelining is disabled while creating the driver then call to renderer_update will block the execution
-		 * on host until rendering commands for this frame finish their execution */
-		renderer_update(driver);
+		driver.update();
 	}
-
-	/* we need to wait for the device to finish any pending tasks, so that all the resources/objects will be unreferenced
-	 * , as test->terminate() destroys the test related objects etc. */
-	renderer_wait_idle(driver);
-
-	renderer_terminate(driver);
-	memory_allocator_destroy(allocator);
-	alloc_terminate();
 	return 0;
 }
