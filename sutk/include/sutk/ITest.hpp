e50c4776b3553dc0c628e30d7ca992b79d2a9cc5
ravi688 <rp0412204@gmail.com>
2024-07-05 03:41:46 +0530

    [SUTK] Added BITMAP_TEXT in SUTK as an example of C++ Binding usage

---
+++ sutk/include/sutk/ITest.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <sge-cpp/sge.hpp>
+
+#include <memory>
+
+namespace SUTK
+{
+	struct DriverInitializationData
+	{
+		SGE::GPUType gpuType;
+		u32 width;
+		u32 height;
+		std::string title;
+		bool fullScreen;
+		bool resizable;
+	};
+
+	class ITest
+	{
+
+	public:
+		static std::unique_ptr<ITest> Create(const std::string& testName);
+
+		virtual DriverInitializationData getInitializationData()
+		{
+			DriverInitializationData data = {
+				SGE::GPUType::Discrete,
+				800,
+				800,
+				"Untitled Test",
+				false,
+				true
+			};
+			return data;
+		}
+		virtual void initialize(SGE::Driver& driver) { }
+		virtual void terminate(SGE::Driver& driver) { }
+		virtual void render(SGE::Driver& driver) { }
+		virtual void update(SGE::Driver& driver, float deltaTime) { }
+	};
+}
---
+++ sutk/include/sutk/tests/BitmapTextTest.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <sutk/ITest.hpp>
+
+#include <sge-cpp/sge.hpp>
+
+#include <sge-cpp/RenderScene.hpp>
+#include <sge-cpp/Font.hpp>
+#include <sge-cpp/BitmapGlyphAtlasTexture.hpp>
+#include <sge-cpp/BitmapText.hpp>
+#include <sge-cpp/RenderScene.hpp>
+#include <sge-cpp/RenderObject.hpp>
+
+namespace SUTK
+{
+	class BitmapTextTest : public ITest
+	{
+	private:
+		SGE::RenderScene m_scene;
+		SGE::Font m_font;
+		SGE::BitmapGlyphAtlasTexture m_texture;
+		SGE::BitmapText m_text;
+		SGE::BitmapTextString m_textString;
+		SGE::BitmapTextString m_anotherString;
+		SGE::RenderObject m_textObject;
+
+	public:
+		BitmapTextTest() = default;
+
+		DriverInitializationData getInitializationData() override;
+
+		void initialize(SGE::Driver& driver) override;
+
+		void terminate(SGE::Driver& driver) override;
+
+		void render(SGE::Driver& driver) override;
+
+		void update(SGE::Driver& driver, float deltaTime) override;
+	};
+}
