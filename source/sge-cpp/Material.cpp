4762d9755b2b638c7d86b54d80c0cf9c4a3d786e
ravi688 <rp0412204@gmail.com>
2024-07-28 04:06:36 +0530

    [SGE] Added SGE::Material::set<vec4_t> template sepecialization

--- source/sge-cpp/Material.cpp
+++ source/sge-cpp/Material.cpp
@@ -7,4 +7,10 @@ namespace SGE
 	{
 		material_set_float(m_handle, name.c_str(), value);
 	}
+
+	template<>
+	void Material::set<vec4_t>(const std::string& name, vec4_t value)
+	{
+		material_set_vec4(m_handle, name.c_str(), value);
+	}
 }
