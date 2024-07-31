e16fe71bdd8082bf6d58cfcc76c999fa2dedc69c
ravi688 <rp0412204@gmail.com>
2024-07-31 23:41:01 +0530

    [SUTK] Added ANCHOR test for testing the anchors

---
+++ sutk/include/sutk/tests/AnchorTest.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <sutk/ITest.hpp>
+
+#include <sge-cpp/sge.hpp>
+
+#include <sutk/UIDriver.hpp>
+
+namespace SUTK
+{
+	class IGfxDriver;
+
+	class AnchorTest : public ITest
+	{
+	private:
+		UIDriver* m_uiDriver;
+		IGfxDriver* m_gfxDriver;
+		RenderRectFill* m_renderRect;
+		RenderRectContainer* m_renderRectContainer;
+
+	public:
+		AnchorTest() : m_uiDriver(NULL), m_gfxDriver(NULL) { }
+
+		DriverInitializationData getInitializationData() override;
+
+		void initialize(SGE::Driver& driver) override;
+
+		void terminate(SGE::Driver& driver) override;
+
+		void render(SGE::Driver& driver) override;
+
+		void update(SGE::Driver& driver, float deltaTime) override;
+	};
+}
--- sutk/source/ITest.cpp
+++ sutk/source/ITest.cpp
@@ -6,6 +6,7 @@
 
 #include <sutk/tests/BitmapTextTest.hpp>
 #include <sutk/tests/TextTest.hpp>
+#include <sutk/tests/AnchorTest.hpp>
 
 namespace SUTK
 {
@@ -15,7 +16,8 @@ namespace SUTK
 	std::unordered_map<std::string, TestCreateCallback> gTestCreateCallbackMap =
 	{
 		{ "BITMAP_TEXT", [] () { return std::unique_ptr<ITest>(new BitmapTextTest()); }},
-		{ "TEXT", [] () { return std::unique_ptr<ITest>(new TextTest()); }}
+		{ "TEXT", [] () { return std::unique_ptr<ITest>(new TextTest()); }},
+		{ "ANCHOR", [] () { return std::unique_ptr<ITest>(new AnchorTest()); }}
 	};
 
 	std::unique_ptr<ITest> ITest::Create(const std::string& testName)
