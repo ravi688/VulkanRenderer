c3dad1bd4059ca98960cdcf7d24b29479310c57a
ravi688 <rp0412204@gmail.com>
2024-08-01 20:41:03 +0530

    [SUTK] Added IInputDriver interface class and a DummyInputDriver implementation

---
+++ sutk/include/sutk/DummyInputDriver.hpp
@@ -0,0 +1,20 @@
+#include <sutk/IInputDriver.hpp> 
+
+namespace SUTK
+{
+	class SUTK_API DummyInputDriver : public IInputDriver
+	{
+	public:
+		// Keyboard
+		virtual bool getKey(KeyCode keycode) noexcept override { return false; }
+		virtual bool getKeyDown(KeyCode keycode) noexcept override { return false; }
+		virtual bool getKeyUp(KeyCode keycode) noexcept override { return false; }
+
+		// Mouse
+		virtual Vec2Df getMousePosition() noexcept override { return { }; }
+		virtual Vec2Df getMouseScrollDelta() noexcept override { return { }; }
+		virtual bool getMouseButton(MouseButton button) noexcept override { return false; }
+		virtual bool getMouseButtonDown(MouseButton button) noexcept override { return false; }
+		virtual bool getMouseButtonUp(MouseButton button) noexcept override { return false; }
+	};
+}
---
+++ sutk/include/sutk/GLFWInputDriver.hpp
@@ -0,0 +1,28 @@
+#include <sutk/IInputDriver.hpp>
+
+namespace SUTK
+{
+	// This typedef taken from glfw3.h
+	typedef struct GLFWwindow GLFWwindow;
+
+	class SUTK_API GLFWInputDriver : public IInputDriver
+	{
+	private:
+		GLFWwindow* m_windowPtr;
+	public:
+
+		GLFWInputDriver(GLFWwindow* windowPtr) noexcept;
+
+		// Keyboard
+		virtual bool getKey(KeyCode keycode) noexcept override;
+		virtual bool getKeyDown(KeyCode keycode) noexcept override;
+		virtual bool getKeyUp(KeyCode keycode) noexcept override;
+
+		// Mouse
+		virtual Vec2Df getMousePosition() noexcept override;
+		virtual Vec2Df getMouseScrollDelta() noexcept override;
+		virtual bool getMouseButton(MouseButton button) noexcept override;
+		virtual bool getMouseButtonDown(MouseButton button) noexcept override;
+		virtual bool getMouseButtonUp(MouseButton button) noexcept override;
+	};
+}
