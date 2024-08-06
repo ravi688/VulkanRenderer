6c119d98a82042a34568cc7437dcdfcb9cb2a673
ravi688 <rp0412204@gmail.com>
2024-08-07 01:22:31 +0530

    [SUTK] Added on key event in SGEInputDriver class

--- sutk/source/IInputDriver.cpp
+++ sutk/source/IInputDriver.cpp
@@ -2,6 +2,161 @@
 
 namespace SUTK
 {
+	std::ostream&& operator <<(std::ostream&& stream, KeyCode v)
+	{
+		auto& _stream = operator <<(stream, v);
+		return std::move(_stream);
+	}
+
+	std::ostream& operator <<(std::ostream& stream, KeyCode v)
+	{
+		switch(v)
+		{
+			case KeyCode::Undefined: stream << "KeyCode::Undefined"; break;
+			case KeyCode::A: stream << "KeyCode::A"; break;
+			case KeyCode::B: stream << "KeyCode::B"; break;
+			case KeyCode::C: stream << "KeyCode::C"; break;
+			case KeyCode::D: stream << "KeyCode::D"; break;
+			case KeyCode::E: stream << "KeyCode::E"; break;
+			case KeyCode::F: stream << "KeyCode::F"; break;
+			case KeyCode::G: stream << "KeyCode::G"; break;
+			case KeyCode::H: stream << "KeyCode::H"; break;
+			case KeyCode::I: stream << "KeyCode::I"; break;
+			case KeyCode::J: stream << "KeyCode::J"; break;
+			case KeyCode::K: stream << "KeyCode::K"; break;
+			case KeyCode::L: stream << "KeyCode::L"; break;
+			case KeyCode::M: stream << "KeyCode::M"; break;
+			case KeyCode::N: stream << "KeyCode::N"; break;
+			case KeyCode::O: stream << "KeyCode::O"; break;
+			case KeyCode::P: stream << "KeyCode::P"; break;
+			case KeyCode::Q: stream << "KeyCode::Q"; break;
+			case KeyCode::R: stream << "KeyCode::R"; break;
+			case KeyCode::S: stream << "KeyCode::S"; break;
+			case KeyCode::T: stream << "KeyCode::T"; break;
+			case KeyCode::U: stream << "KeyCode::U"; break;
+			case KeyCode::V: stream << "KeyCode::V"; break;
+			case KeyCode::W: stream << "KeyCode::W"; break;
+			case KeyCode::X: stream << "KeyCode::X"; break;
+			case KeyCode::Y: stream << "KeyCode::Y"; break;
+			case KeyCode::Z: stream << "KeyCode::Z"; break;
+			case KeyCode::Digit_1_Exclaim: stream << "KeyCode::Digit_1_Exclaim"; break;
+			case KeyCode::Digit_2_Atsign: stream << "KeyCode::Digit_2_Atsign"; break;
+			case KeyCode::Digit_3_Hash: stream << "KeyCode::Digit_3_Hash"; break;
+			case KeyCode::Digit_4_Dollar: stream << "KeyCode::Digit_4_Dollar"; break;
+			case KeyCode::Digit_5_Percent: stream << "KeyCode::Digit_5_Percent"; break;
+			case KeyCode::Digit_6_Hat: stream << "KeyCode::Digit_6_Hat"; break;
+			case KeyCode::Digit_7_Ampersand: stream << "KeyCode::Digit_7_Ampersand"; break;
+			case KeyCode::Digit_8_Asterisk: stream << "KeyCode::Digit_8_Asterisk"; break;
+			case KeyCode::Digit_9_OpenParen: stream << "KeyCode::Digit_9_OpenParen"; break;
+			case KeyCode::Digit_0_CloseParen: stream << "KeyCode::Digit_0_CloseParen"; break;
+			case KeyCode::ReturnEnter: stream << "KeyCode::ReturnEnter"; break;
+			case KeyCode::Escape: stream << "KeyCode::Escape"; break;
+			case KeyCode::DeleteBackspace: stream << "KeyCode::DeleteBackspace"; break;
+			case KeyCode::Tab: stream << "KeyCode::Tab"; break;
+			case KeyCode::Spacebar: stream << "KeyCode::Spacebar"; break;
+			case KeyCode::MinusUnderscore: stream << "KeyCode::MinusUnderscore"; break;
+			case KeyCode::EqualPlus: stream << "KeyCode::EqualPlus"; break;
+			case KeyCode::OpenBracketBrace: stream << "KeyCode::OpenBracketBrace"; break;
+			case KeyCode::CloseBracketBrace: stream << "KeyCode::CloseBracketBrace"; break;
+			case KeyCode::BackslashPillor: stream << "KeyCode::BackslashPillor"; break;
+			case KeyCode::Tilde: stream << "KeyCode::Tilde"; break;
+			case KeyCode::SemicolonColon: stream << "KeyCode::SemicolonColon"; break;
+			case KeyCode::QuoteSingleDouble: stream << "KeyCode::QuoteSingleDouble"; break;
+			case KeyCode::Tilde2: stream << "KeyCode::Tilde2"; break;
+			case KeyCode::CommaLessThan: stream << "KeyCode::CommaLessThan"; break;
+			case KeyCode::FullstopGreaterThan: stream << "KeyCode::FullstopGreaterThan"; break;
+			case KeyCode::ForwardSlashQuestion: stream << "KeyCode::ForwardSlashQuestion"; break;
+			case KeyCode::Capslock: stream << "KeyCode::Capslock"; break;
+			case KeyCode::F1: stream << "KeyCode::F1"; break;
+			case KeyCode::F2: stream << "KeyCode::F2"; break;
+			case KeyCode::F3: stream << "KeyCode::F3"; break;
+			case KeyCode::F4: stream << "KeyCode::F4"; break;
+			case KeyCode::F5: stream << "KeyCode::F5"; break;
+			case KeyCode::F6: stream << "KeyCode::F6"; break;
+			case KeyCode::F7: stream << "KeyCode::F7"; break;
+			case KeyCode::F8: stream << "KeyCode::F8"; break;
+			case KeyCode::F9: stream << "KeyCode::F9"; break;
+			case KeyCode::F10: stream << "KeyCode::F10"; break;
+			case KeyCode::F11: stream << "KeyCode::F11"; break;
+			case KeyCode::F12: stream << "KeyCode::F12"; break;
+			case KeyCode::PrintScreen: stream << "KeyCode::PrintScreen"; break;
+			case KeyCode::ScrollLock: stream << "KeyCode::ScrollLock"; break;
+			case KeyCode::Pause: stream << "KeyCode::Pause"; break;
+			case KeyCode::Insert: stream << "KeyCode::Insert"; break;
+			case KeyCode::Home: stream << "KeyCode::Home"; break;
+			case KeyCode::PageUp: stream << "KeyCode::PageUp"; break;
+			case KeyCode::DeleteForward: stream << "KeyCode::DeleteForward"; break;
+			case KeyCode::End: stream << "KeyCode::End"; break;
+			case KeyCode::PageDown: stream << "KeyCode::PageDown"; break;
+			case KeyCode::RightArrow: stream << "KeyCode::RightArrow"; break;
+			case KeyCode::LeftArrow: stream << "KeyCode::LeftArrow"; break;
+			case KeyCode::DownArrow: stream << "KeyCode::DownArrow"; break;
+			case KeyCode::UpArrow: stream << "KeyCode::UpArrow"; break;
+			case KeyCode::KeypadNumlockClear: stream << "KeyCode::KeypadNumlockClear"; break;
+			case KeyCode::KeypadForwardSlash: stream << "KeyCode::KeypadForwardSlash"; break;
+			case KeyCode::KeypadAsterisk: stream << "KeyCode::KeypadAsterisk"; break;
+			case KeyCode::KeypadMinus: stream << "KeyCode::KeypadMinus"; break;
+			case KeyCode::KeypadPlus: stream << "KeyCode::KeypadPlus"; break;
+			case KeyCode::KeypadEnter: stream << "KeyCode::KeypadEnter"; break;
+			case KeyCode::Keypad1End: stream << "KeyCode::Keypad1End"; break;
+			case KeyCode::Keypad2DownArrow: stream << "KeyCode::Keypad2DownArrow"; break;
+			case KeyCode::Keypad3PageDown: stream << "KeyCode::Keypad3PageDown"; break;
+			case KeyCode::Keypad4LeftArrow: stream << "KeyCode::Keypad4LeftArrow"; break;
+			case KeyCode::Keypad5: stream << "KeyCode::Keypad5"; break;
+			case KeyCode::Keypad6RightArrow: stream << "KeyCode::Keypad6RightArrow"; break;
+			case KeyCode::Keypad7Home: stream << "KeyCode::Keypad7Home"; break;
+			case KeyCode::Keypad8UpArrow: stream << "KeyCode::Keypad8UpArrow"; break;
+			case KeyCode::Keypad9PageUp: stream << "KeyCode::Keypad9PageUp"; break;
+			case KeyCode::Keypad0Insert: stream << "KeyCode::Keypad0Insert"; break;
+			case KeyCode::KeypadDotDelete: stream << "KeyCode::KeypadDotDelete"; break;
+			case KeyCode::BackslashPillorNearLeftShift: stream << "KeyCode::BackslashPillorNearLeftShift"; break;
+			case KeyCode::Application: stream << "KeyCode::Application"; break;
+			case KeyCode::Power: stream << "KeyCode::Power"; break;
+			case KeyCode::KeypadEqual: stream << "KeyCode::KeypadEqual"; break;
+			case KeyCode::F13: stream << "KeyCode::F13"; break;
+			case KeyCode::F14: stream << "KeyCode::F14"; break;
+			case KeyCode::F15: stream << "KeyCode::F15"; break;
+			case KeyCode::F16: stream << "KeyCode::F16"; break;
+			case KeyCode::F17: stream << "KeyCode::F17"; break;
+			case KeyCode::F18: stream << "KeyCode::F18"; break;
+			case KeyCode::F19: stream << "KeyCode::F19"; break;
+			case KeyCode::F20: stream << "KeyCode::F20"; break;
+			case KeyCode::F21: stream << "KeyCode::F21"; break;
+			case KeyCode::F22: stream << "KeyCode::F22"; break;
+			case KeyCode::F23: stream << "KeyCode::F23"; break;
+			case KeyCode::F24: stream << "KeyCode::F24"; break;
+			case KeyCode::F25: stream << "KeyCode::F25"; break;
+			case KeyCode::Execute: stream << "KeyCode::Execute"; break;
+			case KeyCode::Help: stream << "KeyCode::Help"; break;
+			case KeyCode::Menu: stream << "KeyCode::Menu"; break;
+			case KeyCode::Select: stream << "KeyCode::Select"; break;
+			case KeyCode::Stop: stream << "KeyCode::Stop"; break;
+			case KeyCode::Again: stream << "KeyCode::Again"; break;
+			case KeyCode::Undo: stream << "KeyCode::Undo"; break;
+			case KeyCode::Cut: stream << "KeyCode::Cut"; break;
+			case KeyCode::Copy: stream << "KeyCode::Copy"; break;
+			case KeyCode::Paste: stream << "KeyCode::Paste"; break;
+			case KeyCode::Find: stream << "KeyCode::Find"; break;
+			case KeyCode::Mute: stream << "KeyCode::Mute"; break;
+			case KeyCode::VolumeUp: stream << "KeyCode::VolumeUp"; break;
+			case KeyCode::VolumeDown: stream << "KeyCode::VolumeDown"; break;
+			case KeyCode::Numlock: stream << "KeyCode::Numlock"; break;
+			case KeyCode::KeypadComma: stream << "KeyCode::KeypadComma"; break;
+			case KeyCode::KeypadEqualSign: stream << "KeyCode::KeypadEqualSign"; break;
+			case KeyCode::LeftControl: stream << "KeyCode::LeftControl"; break;
+			case KeyCode::LeftShift: stream << "KeyCode::LeftShift"; break;
+			case KeyCode::LeftAlt: stream << "KeyCode::LeftAlt"; break;
+			case KeyCode::LeftGui: stream << "KeyCode::LeftGui"; break;
+			case KeyCode::RightControl: stream << "KeyCode::RightControl"; break;
+			case KeyCode::RightShift: stream << "KeyCode::RightShift"; break;
+			case KeyCode::RightAlt: stream << "KeyCode::RightAlt"; break;
+			case KeyCode::RightGui: stream << "KeyCode::RightGui"; break;
+			default:
+				debug_log_error("KeyCode can't be formatted into a string");
+		}
+		return stream;
+	}
+
 	std::ostream& operator <<(std::ostream& stream, MouseButton v)
 	{
 		switch(v)
--- sutk/source/SGEInputDriver.cpp
+++ sutk/source/SGEInputDriver.cpp
@@ -26,6 +26,153 @@ namespace SUTK
 		}
 	}
 
+	static KeyCode getKeyCode(key_code_t keycode)
+	{
+		switch(keycode)
+		{
+			case KEY_CODE_A: return KeyCode::A;
+			case KEY_CODE_B: return KeyCode::B;
+			case KEY_CODE_C: return KeyCode::C;
+			case KEY_CODE_D: return KeyCode::D;
+			case KEY_CODE_E: return KeyCode::E;
+			case KEY_CODE_F: return KeyCode::F;
+			case KEY_CODE_G: return KeyCode::G;
+			case KEY_CODE_H: return KeyCode::H;
+			case KEY_CODE_I: return KeyCode::I;
+			case KEY_CODE_J: return KeyCode::J;
+			case KEY_CODE_K: return KeyCode::K;
+			case KEY_CODE_L: return KeyCode::L;
+			case KEY_CODE_M: return KeyCode::M;
+			case KEY_CODE_N: return KeyCode::N;
+			case KEY_CODE_O: return KeyCode::O;
+			case KEY_CODE_P: return KeyCode::P;
+			case KEY_CODE_Q: return KeyCode::Q;
+			case KEY_CODE_R: return KeyCode::R;
+			case KEY_CODE_S: return KeyCode::S;
+			case KEY_CODE_T: return KeyCode::T;
+			case KEY_CODE_U: return KeyCode::U;
+			case KEY_CODE_V: return KeyCode::V;
+			case KEY_CODE_W: return KeyCode::W;
+			case KEY_CODE_X: return KeyCode::X;
+			case KEY_CODE_Y: return KeyCode::Y;
+			case KEY_CODE_Z: return KeyCode::Z;
+			case KEY_CODE_1_EXCLAIM: return KeyCode::Digit_1_Exclaim;
+			case KEY_CODE_2_ATSIGN: return KeyCode::Digit_2_Atsign;
+			case KEY_CODE_3_HASH: return KeyCode::Digit_3_Hash;
+			case KEY_CODE_4_DOLLAR: return KeyCode::Digit_4_Dollar;
+			case KEY_CODE_5_PERCENT: return KeyCode::Digit_5_Percent;
+			case KEY_CODE_6_HAT: return KeyCode::Digit_6_Hat;
+			case KEY_CODE_7_AMPERSAND: return KeyCode::Digit_7_Ampersand;
+			case KEY_CODE_8_ASTERISK: return KeyCode::Digit_8_Asterisk;
+			case KEY_CODE_9_OPEN_PAREN: return KeyCode::Digit_9_OpenParen;
+			case KEY_CODE_0_CLOSE_PAREN: return KeyCode::Digit_0_CloseParen;
+			case KEY_CODE_RETURN_ENTER: return KeyCode::ReturnEnter;
+			case KEY_CODE_ESCAPE: return KeyCode::Escape;
+			case KEY_CODE_DELETE_BACKSPACE: return KeyCode::DeleteBackspace;
+			case KEY_CODE_TAB: return KeyCode::Tab;
+			case KEY_CODE_SPACEBAR: return KeyCode::Spacebar;
+			case KEY_CODE_MINUS_UNDERSCORE: return KeyCode::MinusUnderscore;
+			case KEY_CODE_EQUAL_PLUS: return KeyCode::EqualPlus;
+			case KEY_CODE_OPEN_BRACKET_BRACE: return KeyCode::OpenBracketBrace;
+			case KEY_CODE_CLOSE_BRACKET_BRACE: return KeyCode::CloseBracketBrace;
+			case KEY_CODE_BACKSLASH_PILLOR: return KeyCode::BackslashPillor;
+			case KEY_CODE_TILDE: return KeyCode::Tilde;
+			case KEY_CODE_SEMICOLON_COLON: return KeyCode::SemicolonColon;
+			case KEY_CODE_QUOTE_SINGLE_DOUBLE: return KeyCode::QuoteSingleDouble;
+			case KEY_CODE_TILDE2: return KeyCode::Tilde2;
+			case KEY_CODE_COMMA_LESS_THAN: return KeyCode::CommaLessThan;
+			case KEY_CODE_FULLSTOP_GREATER_THAN: return KeyCode::FullstopGreaterThan;
+			case KEY_CODE_FORWARD_SLASH_QUESTION: return KeyCode::ForwardSlashQuestion;
+			case KEY_CODE_CAPSLOCK: return KeyCode::Capslock;
+			case KEY_CODE_F1: return KeyCode::F1;
+			case KEY_CODE_F2: return KeyCode::F2;
+			case KEY_CODE_F3: return KeyCode::F3;
+			case KEY_CODE_F4: return KeyCode::F4;
+			case KEY_CODE_F5: return KeyCode::F5;
+			case KEY_CODE_F6: return KeyCode::F6;
+			case KEY_CODE_F7: return KeyCode::F7;
+			case KEY_CODE_F8: return KeyCode::F8;
+			case KEY_CODE_F9: return KeyCode::F9;
+			case KEY_CODE_F10: return KeyCode::F10;
+			case KEY_CODE_F11: return KeyCode::F11;
+			case KEY_CODE_F12: return KeyCode::F12;
+			case KEY_CODE_PRINTSCREEN: return KeyCode::PrintScreen;
+			case KEY_CODE_SCROLLLOCK: return KeyCode::ScrollLock;
+			case KEY_CODE_PAUSE: return KeyCode::Pause;
+			case KEY_CODE_INSERT: return KeyCode::Insert;
+			case KEY_CODE_HOME: return KeyCode::Home;
+			case KEY_CODE_PAGE_UP: return KeyCode::PageUp;
+			case KEY_CODE_DELETE_FORWARD: return KeyCode::DeleteForward;
+			case KEY_CODE_END: return KeyCode::End;
+			case KEY_CODE_PAGE_DOWN: return KeyCode::PageDown;
+			case KEY_CODE_RIGHT_ARROW: return KeyCode::RightArrow;
+			case KEY_CODE_LEFT_ARROW: return KeyCode::LeftArrow;
+			case KEY_CODE_DOWN_ARROW: return KeyCode::DownArrow;
+			case KEY_CODE_UP_ARROW: return KeyCode::UpArrow;
+			case KEY_CODE_KEYPAD_NUMLOCK_CLEAR: return KeyCode::KeypadNumlockClear;
+			case KEY_CODE_KEYPAD_FORWARD_SLASH: return KeyCode::KeypadForwardSlash;
+			case KEY_CODE_KEYPAD_ASTERISK: return KeyCode::KeypadAsterisk;
+			case KEY_CODE_KEYPAD_MINUS: return KeyCode::KeypadMinus;
+			case KEY_CODE_KEYPAD_PLUS: return KeyCode::KeypadPlus;
+			case KEY_CODE_KEYPAD_ENTER: return KeyCode::KeypadEnter;
+			case KEY_CODE_KEYPAD_1_END: return KeyCode::Keypad1End;
+			case KEY_CODE_KEYPAD_2_DOWN_ARROW: return KeyCode::Keypad2DownArrow;
+			case KEY_CODE_KEYPAD_3_PAGE_DOWN: return KeyCode::Keypad3PageDown;
+			case KEY_CODE_KEYPAD_4_LEFT_ARROW: return KeyCode::Keypad4LeftArrow;
+			case KEY_CODE_KEYPAD_5: return KeyCode::Keypad5;
+			case KEY_CODE_KEYPAD_6_RIGHT_ARROW: return KeyCode::Keypad6RightArrow;
+			case KEY_CODE_KEYPAD_7_HOME: return KeyCode::Keypad7Home;
+			case KEY_CODE_KEYPAD_8_UP_ARROW: return KeyCode::Keypad8UpArrow;
+			case KEY_CODE_KEYPAD_9_PAGE_UP: return KeyCode::Keypad9PageUp;
+			case KEY_CODE_KEYPAD_0_INSERT: return KeyCode::Keypad0Insert;
+			case KEY_CODE_KEYPAD_DOT_DELETE: return KeyCode::KeypadDotDelete;
+			case KEY_CODE_BACKSLASH_PILLOR_NEAR_LEFT_SHIFT: return KeyCode::BackslashPillorNearLeftShift;
+			case KEY_CODE_APPLICATION: return KeyCode::Application;
+			case KEY_CODE_POWER: return KeyCode::Power;
+			case KEY_CODE_KEYPAD_EQUAL: return KeyCode::KeypadEqual;
+			case KEY_CODE_F13: return KeyCode::F13;
+			case KEY_CODE_F14: return KeyCode::F14;
+			case KEY_CODE_F15: return KeyCode::F15;
+			case KEY_CODE_F16: return KeyCode::F16;
+			case KEY_CODE_F17: return KeyCode::F17;
+			case KEY_CODE_F18: return KeyCode::F18;
+			case KEY_CODE_F19: return KeyCode::F19;
+			case KEY_CODE_F20: return KeyCode::F20;
+			case KEY_CODE_F21: return KeyCode::F21;
+			case KEY_CODE_F22: return KeyCode::F22;
+			case KEY_CODE_F23: return KeyCode::F23;
+			case KEY_CODE_F24: return KeyCode::F24;
+			case KEY_CODE_EXECUTE: return KeyCode::Execute;
+			case KEY_CODE_HELP: return KeyCode::Help;
+			case KEY_CODE_MENU: return KeyCode::Menu;
+			case KEY_CODE_SELECT: return KeyCode::Select;
+			case KEY_CODE_STOP: return KeyCode::Stop;
+			case KEY_CODE_AGAIN: return KeyCode::Again;
+			case KEY_CODE_UNDO: return KeyCode::Undo;
+			case KEY_CODE_CUT: return KeyCode::Cut;
+			case KEY_CODE_COPY: return KeyCode::Copy;
+			case KEY_CODE_PASTE: return KeyCode::Paste;
+			case KEY_CODE_FIND: return KeyCode::Find;
+			case KEY_CODE_MUTE: return KeyCode::Mute;
+			case KEY_CODE_VOLUME_UP: return KeyCode::VolumeUp;
+			case KEY_CODE_VOLUME_DOWN: return KeyCode::VolumeDown;
+			case KEY_CODE_NUMLOCK: return KeyCode::Numlock;
+			case KEY_CODE_KEYPAD_COMMA: return KeyCode::KeypadComma;
+			case KEY_CODE_KEYPAD_EQUAL_SIGN: return KeyCode::KeypadEqualSign;
+			case KEY_CODE_LEFT_CONTROL: return KeyCode::LeftControl;
+			case KEY_CODE_LEFT_SHIFT: return KeyCode::LeftShift;
+			case KEY_CODE_LEFT_ALT: return KeyCode::LeftAlt;
+			case KEY_CODE_LEFT_GUI: return KeyCode::LeftGui;
+			case KEY_CODE_RIGHT_CONTROL: return KeyCode::RightControl;
+			case KEY_CODE_RIGHT_SHIFT: return KeyCode::RightShift;
+			case KEY_CODE_RIGHT_ALT: return KeyCode::RightAlt;
+			case KEY_CODE_RIGHT_GUI: return KeyCode::RightGui;
+			default:
+				debug_log_error("Key unrecognized in SUTK");
+				return KeyCode::Undefined;
+		}
+	}
+
 	SGEInputDriver::SGEInputDriver(SGE::Driver& driver) noexcept : m_window(driver.getRenderWindow())
 	{
 		getOnCursorMoveEvent().setPublisher(this);
@@ -55,6 +202,15 @@ namespace SUTK
 			auto window = inputDriver->m_window;
 			auto delta = window.getScrollDelta();
 			inputDriver->getOnScrollEvent().publish({ delta.x, delta.y });
+		}, reinterpret_cast<void*>(this));
+
+		getOnKeyEvent().setPublisher(this);
+		m_window.getOnKeyEvent().subscribe([](void* publisherData, void* handlerData)
+		{
+			auto* inputDriver = reinterpret_cast<SGEInputDriver*>(handlerData);
+			auto data = reinterpret_cast<event_publisher_arg_data_t*>(publisherData);
+			auto keyData = reinterpret_cast<key_event_data_t*>(data->arg);
+			inputDriver->getOnKeyEvent().publish(getKeyCode(keyData->keycode), SUTK::getKeyEventType(keyData->event_type));
 		}, reinterpret_cast<void*>(this));
 	}
 
