f92980803b9020a5f788845dd9db80b1bef65871
ravi688 <rp0412204@gmail.com>
2024-08-08 06:07:50 +0530

    [SUTK] mouse event callbacks should be invoked only in the local coordinates of a container

--- sutk/include/sutk/InputEventHandlerObject.hpp
+++ sutk/include/sutk/InputEventHandlerObject.hpp
@@ -1,5 +1,6 @@
 #include <sutk/defines.hpp> // for SUTK_API
 #include <sutk/IInputDriver.hpp> // for SUTK::MouseButton and SUTK::KeyEvent
+#include <sutk/Container.hpp> // for SUTK::Container
 
 namespace SUTK
 {
@@ -48,6 +49,27 @@ namespace SUTK
 		}
 	};
 
+	template<typename InputEventType>
+	class TInputEventHandlerContainerObject : public TInputEventHandlerObject<InputEventType>
+	{
+	private:
+		Container* m_container;
+
+	protected:
+		TInputEventHandlerContainerObject(InputEventType& event) noexcept : TInputEventHandlerObject<InputEventType>(event), m_container(NULL) { }
+		TInputEventHandlerContainerObject(InputEventType& event, Container* container) noexcept : TInputEventHandlerObject<InputEventType>(event), m_container(container) { }
+		bool isInside(Vec2Df point) const noexcept
+		{
+			return (m_container != NULL) ? m_container->containsGlobalCoords(point) : true;
+		}
+		Container* getContainer() noexcept { return m_container; }
+	};
+
+	// The onMouseEnter and onMouseExit are called only for the render window's entire rect
+	// Therefore, this class is not derived from TInputEventHandlerContainerObject
+	// If you want to get callbacks on mouse enter and exit across the boundaries of a specific Container
+	// then derive your class from MouseMoveHandlerObject and call at least once the enableMouseEnter and enableMouseExit with 'true' values
+	// to enable the callbacks.
 	class MouseEnterExitHandlerObject : public TInputEventHandlerObject<IInputDriver::OnMouseEnterExitEvent>
 	{
 	protected:
@@ -60,34 +82,48 @@ namespace SUTK
 		virtual ~MouseEnterExitHandlerObject() noexcept = default;
 	};
 
-	class MouseMoveHandlerObject : public TInputEventHandlerObject<IInputDriver::OnMouseMoveEvent>
+	#define MOUSE_INPUT_HANDLER_CTOR_DECLARE(className)\
+		className(UIDriver& driver) noexcept : className(driver, NULL) { }\
+		className(UIDriver& driver, Container* container) noexcept;\
+
+	class MouseMoveHandlerObject : public TInputEventHandlerContainerObject<IInputDriver::OnMouseMoveEvent>
 	{
+	private:
+		bool m_isMouseEnterEnabled;
+		bool m_isMouseExitEnabled;
+		bool m_isInside;
 	protected:
 		virtual void onMouseMove(Vec2Df position) = 0;
 
-		MouseMoveHandlerObject(UIDriver& driver) noexcept;
+		MOUSE_INPUT_HANDLER_CTOR_DECLARE(MouseMoveHandlerObject)
+
+		virtual void onMouseEnter() noexcept { }
+		virtual void onMouseExit() noexcept { }
+
+		void enableMouseEnter(bool isEnable) noexcept { m_isMouseEnterEnabled = isEnable; }
+		void enableMouseExit(bool isEnable) noexcept { m_isMouseExitEnabled = isEnable; }
 
 	public:
 		virtual ~MouseMoveHandlerObject() noexcept = default;
 	};
 
-	class MouseClickHandlerObject : public TInputEventHandlerObject<IInputDriver::OnMouseButtonEvent>
+	class MouseClickHandlerObject : public TInputEventHandlerContainerObject<IInputDriver::OnMouseButtonEvent>
 	{
 	protected:
 		virtual void onMouseClick(MouseButton button, KeyEvent action) = 0;
 
-		MouseClickHandlerObject(UIDriver& driver) noexcept;
+		MOUSE_INPUT_HANDLER_CTOR_DECLARE(MouseClickHandlerObject)
 
 	public:
 		virtual ~MouseClickHandlerObject() noexcept = default;
 	};
 
-	class MouseScrollHandlerObject : public TInputEventHandlerObject<IInputDriver::OnMouseScrollEvent>
+	class MouseScrollHandlerObject : public TInputEventHandlerContainerObject<IInputDriver::OnMouseScrollEvent>
 	{
 	protected:
 		virtual void onMouseScroll(Vec2Df scrollDelta) = 0;
 
-		MouseScrollHandlerObject(UIDriver& driver) noexcept;
+		MOUSE_INPUT_HANDLER_CTOR_DECLARE(MouseScrollHandlerObject)
 
 	public:
 		virtual ~MouseScrollHandlerObject() noexcept = default;
