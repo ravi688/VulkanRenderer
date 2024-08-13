17427ef6f343d36b5983b67c5650a8152aaf2408
ravi688 <rp0412204@gmail.com>
2024-08-14 02:58:43 +0530

    [SUTK] Modified default color attachment blend state values

--- toolchain/shader_compiler/source/compiler/codegen/gfx_pipeline.c
+++ toolchain/shader_compiler/source/compiler/codegen/gfx_pipeline.c
@@ -460,8 +460,14 @@ static void setup_default_values(UserData* data)
 {
 	data->defaultColorBlendAttachmentState = (VkPipelineColorBlendAttachmentState)
 	{
+		.blendEnable = VK_FALSE,
+		.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
+		.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
+		.colorBlendOp = VK_BLEND_OP_ADD,
+		.srcAlphaBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
+		.dstAlphaBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
+		.alphaBlendOp = VK_BLEND_OP_ADD,
 		.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT,
-		.blendEnable = VK_FALSE
 	};
 
 	data->defaultScissor = (VkRect2D)
