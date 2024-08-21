c0c63682c48ec147d46e925a2ef996d2516e116e
ravi688 <rp0412204@gmail.com>
2024-08-22 04:26:23 +0530

    [SUTK] Added SUTK::Button and other new associated classes

---
+++ sutk/include/sutk/tests/ButtonTest.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <sutk/ITest.hpp>
+
+#include <sge-cpp/sge.hpp>
+
+#include <sutk/UIDriver.hpp>
+
+namespace SUTK
+{
+	class IGfxDriver;
+	class Button;
+
+	class ButtonTest : public ITest
+	{
+	private:
+		UIDriver* m_uiDriver;
+		IGfxDriver* m_gfxDriver;
+		IInputDriver* m_inputDriver;
+		Button* m_button;
+
+	public:
+		ButtonTest() : m_uiDriver(NULL), m_gfxDriver(NULL) { }
+
+		DriverInitializationData getInitializationData() override;
+
+		void initialize(SGE::Driver& driver) override;
+
+		void terminate(SGE::Driver& driver) override;
+
+		void render(SGE::Driver& driver) override;
+
+		void update(SGE::Driver& driver, float deltaTime) override;
+	};
+}
---
+++ sutk/source/Button.cpp
@@ -0,0 +1,147 @@
+#include <sutk/Button.hpp>
+
+namespace SUTK
+{
+	VisualButtonRect::VisualButtonRect(SUTK::UIDriver& driver, SUTK::RenderableContainer* container) noexcept : RenderRectFillRound(driver, container),
+																							  m_cta({ { State::Hover, Color4::grey(0.7f) },
+																							  		  { State::Press, Color4::grey(0.3f) },
+																							  		  { State::Idle, Color4::grey(1.0f) } })
+	{
+		m_cta.setDefault(State::Idle);
+	}
+
+	bool VisualButtonRect::isDirty() noexcept
+	{
+		return m_cta.isRunning() || RenderRectFillRound::isDirty();
+	}
+	void VisualButtonRect::update() noexcept
+	{
+		if(m_cta.isRunning())
+		{
+			m_cta.update();
+			setColor(m_cta.getColor());
+		}
+		RenderRectFillRound::update();
+	}
+
+	void VisualButtonRect::setTransitionDelay(const f32 transitionDelay) noexcept
+	{
+		m_cta.setTransitionDelay(transitionDelay);
+	}
+
+	void VisualButtonRect::setHoverColor(Color4 color) noexcept
+	{
+		m_cta.set(State::Hover, color);
+	}
+	void VisualButtonRect::setPressColor(Color4 color) noexcept
+	{
+		m_cta.set(State::Press, color);
+	}
+	void VisualButtonRect::setIdleColor(Color4 color) noexcept
+	{
+		m_cta.set(State::Idle,  color);
+		m_cta.setDefault(State::Idle);
+		setColor(m_cta.getColor());
+	}
+	void VisualButtonRect::setState(State state) noexcept
+	{
+		m_cta.transitionTo(state);
+	}
+
+
+	DefaultButtonGraphic::DefaultButtonGraphic(UIDriver& driver, Container* parent) noexcept : RenderableContainer(driver, parent)
+	{
+		_com_assert(parent != NULL);
+		
+		// size of this graphic should be as that of Button's rect
+		setRect({ { 0, 0 }, parent->getSize() });
+		getAnchorRect()->setRect( { 0, 0, 1, 1 });
+
+		m_visualButton = driver.createRenderable<VisualButtonRect>(this);
+	}
+
+	/*						____________
+						  \|/			|
+		Idle Color --> Hover Color --> Press Color
+		   /|\				|
+			|_______________|
+
+	 */
+
+	void DefaultButtonGraphic::onHover(HoverInfo info) noexcept
+	{
+		if(info.isEnter)
+			m_visualButton->setState(VisualButtonRect::State::Hover);
+		else if(info.isExit)
+			m_visualButton->setState(VisualButtonRect::State::Idle);
+	}
+	void DefaultButtonGraphic::onPress() noexcept
+	{
+		m_visualButton->setState(VisualButtonRect::State::Press);
+	}
+	void DefaultButtonGraphic::onRelease() noexcept
+	{
+		m_visualButton->setState(VisualButtonRect::State::Hover);
+	}
+
+	Button::Button(UIDriver& driver, Container* parent, bool isCreateDefaultGraphic) noexcept : Container(driver, parent),
+																									MouseMoveHandlerObject(driver, this),
+																									MouseClickHandlerObject(driver, this),
+																									m_graphic(NULL)
+	{ 
+		if(isCreateDefaultGraphic)
+			m_graphic = driver.createContainer<DefaultButtonGraphic>(this);
+		enableMouseEnter(true);
+		enableMouseExit(true);
+	}
+
+	void Button::onMouseEnter() noexcept
+	{
+		if(m_graphic != NULL)
+		{
+			m_onEnterEvent.publish();
+			HoverInfo info { };
+			info.position = getScreenCoordsToLocalCoords(getInputDriver().getMousePosition());
+			info.isEnter = true;
+			m_graphic->onHover(info);
+		}
+	}
+
+	void Button::onMouseExit() noexcept
+	{
+		if(m_graphic != NULL)
+		{
+			m_onExitEvent.publish();
+			HoverInfo info { };
+			info.position = getScreenCoordsToLocalCoords(getInputDriver().getMousePosition());
+			info.isExit = true;
+			m_graphic->onHover(info);
+		}
+	}
+
+	void Button::onMouseMove(Vec2Df position) noexcept
+	{
+		if(m_graphic != NULL)
+		{
+			HoverInfo info { };
+			info.position = position;
+			m_graphic->onHover(info);
+		}
+	}
+
+	void Button::onMouseClick(MouseButton button, KeyEvent action) noexcept
+	{
+		if(button != MouseButton::Left)
+			return;
+		if(action == KeyEvent::Press)
+		{
+			m_onPressEvent.publish();
+			m_graphic->onPress();
+		}
+		else if(action == KeyEvent::Release)
+		{
+			m_onReleaseEvent.publish();
+			m_graphic->onRelease();
+		}
+	}
+}
