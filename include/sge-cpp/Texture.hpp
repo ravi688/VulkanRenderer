05af2719779cc327f5f477666426b47a9866732c
ravi688 <rp0412204@gmail.com>
2024-09-19 23:16:41 +0530

    [SGE] Added Texture constructor overload to load from a file path

--- include/sge-cpp/Texture.hpp
+++ include/sge-cpp/Texture.hpp
@@ -3,14 +3,18 @@
 #include <sge/texture.h> /* for texture_t* */
 #include <common/PtrReference.hpp> // for com::PtrReference
 
+#include <string> // for std::string
+
 namespace SGE
 {
+	class Driver;
 	class SGE_API Texture : public com::PtrReference<texture_t>
 	{
 		PTR_REFERENCE_DERIVED_CLASS(Texture);
 
 		friend class BitmapGlyphAtlasTexture;
 	public:
+		Texture(Driver& driver, texture_type_t type, const std::string& str) noexcept;
 		void destroy() noexcept
 		{
 			texture_destroy(m_handle);
