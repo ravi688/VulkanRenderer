b9e2471a5a55588797b74c270ba3dbfbcca39313
ravi688 <rp0412204@gmail.com>
2024-09-19 23:18:12 +0530

    [SGE] Added image_rect.v3dshader and embedded it into shaders.h

--- include/sge/pygen/gen_shaders.sh
+++ include/sge/pygen/gen_shaders.sh
@@ -3,6 +3,7 @@
 # TODO: move this to GNU makefile automaticaly generate shaders.h file whenever any of the following files changes.
 
 python $(pwd)/$(dirname "$0")/gen_shaders.py \
+		../../../shaders/presets/image_rect.v3dshader \
 		../../../shaders/presets/solid_color.v3dshader \
 		../../../shaders/presets/solid_color_rect_array.v3dshader \
 		../../../shaders/presets/no_standalone_use/sdf_template.template \
