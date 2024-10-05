329d5072142b69fd3922056347ea099f37dbac37
ravi688 <rp0412204@gmail.com>
2024-10-06 05:05:22 +0530

    [SUTK] Added SUTK::ScrollToggleButtonGroupView

---
+++ sutk/include/sutk/ScrollToggleButtonGroupView.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <sutk/ToggleButtonGroupView.hpp>
+#include <sutk/MaskedScrollableContainer.hpp>
+
+namespace SUTK
+{
+	class SUTK_API ScrollToggleButtonGroupView : public MaskedScrollableContainer<ToggleButtonGroupView>
+	{
+	protected:
+		virtual ToggleButton* onCreate() noexcept override;
+		virtual void onParentResize(const SUTK::Rect2Df& newRect, bool isPositionChanged, bool isSizeChanged) override;
+	public:
+		ScrollToggleButtonGroupView(UIDriver& driver, Container* parent, u32 poolSize = 7, GfxDriverObjectHandleType textGroup = GFX_DRIVER_OBJECT_NULL_HANDLE) noexcept;
+	};
+}
