3741dad51628f3039c74a54a2b9cc92e64897243
ravi688 <rp0412204@gmail.com>
2024-10-09 03:14:34 +0530

    [SUTK] Added CONTAINER test

---
+++ sutk/include/sutk/tests/ContainerTest.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <sutk/ITest.hpp>
+
+#include <sge-cpp/sge.hpp>
+
+#include <sutk/UIDriver.hpp>
+
+namespace SUTK
+{
+	class IGfxDriver;
+
+	class ContainerTest : public ITest
+	{
+	private:
+		UIDriver* m_uiDriver;
+		IInputDriver* m_inputDriver;
+		IGfxDriver* m_gfxDriver;
+
+	public:
+		ContainerTest() : m_uiDriver(NULL), m_inputDriver(NULL), m_gfxDriver(NULL) { }
+
+		DriverInitializationData getInitializationData() override;
+
+		void initialize(SGE::Driver& driver) override;
+
+		void terminate(SGE::Driver& driver) override;
+
+		void render(SGE::Driver& driver) override;
+
+		void update(SGE::Driver& driver, float deltaTime) override;
+	};
+}
---
+++ sutk/source/tests/ContainerTest.cpp
@@ -0,0 +1,59 @@
+#include <sutk/tests/ContainerTest.hpp>
+
+#include <sutk/RenderRectFill.hpp>
+#include <sutk/SGEGfxDriver.hpp>
+#include <sutk/SGEInputDriver.hpp>
+#include <sutk/RenderableContainer.hpp>
+#include <sutk/FullWindowContainer.hpp>
+
+namespace SUTK
+{
+	DriverInitializationData ContainerTest::getInitializationData()
+	{
+		auto data = ITest::getInitializationData();
+		data.title = "Container Test";
+		return data;
+	}
+
+	void ContainerTest::initialize(SGE::Driver& driver)
+	{
+		m_gfxDriver = new SGEGfxDriver(driver);
+		m_inputDriver = new SGEInputDriver(driver);
+		m_uiDriver = new UIDriver(*m_gfxDriver);
+		FullWindowContainer* rootContainer = m_uiDriver->createContainer<FullWindowContainer>(NULL);
+
+		Container* c1 = m_uiDriver->createContainer<Container>(rootContainer);
+		c1->enableDebug();
+		c1->setPosition({ 1, 1 });
+		Container* c2 = m_uiDriver->createContainer<Container>(NULL);
+		c2->setRect({ 2, 2, 6, 6 });
+		c2->setParent(c1);
+		c2->getAnchorRect()->fitToParentRect();
+		c2->enableDebug(true, Color4::yellow());
+		c1->getAnchorRect()->fitToParentRect();
+
+		m_inputDriver->getOnKeyEvent().subscribe([c2](IInputDriver*, KeyCode keycode, KeyEvent action, ModifierKeys)
+		{
+			if(action == KeyEvent::Press)
+			{
+				std::cout << c2->getRect() << std::endl;
+			}
+		});
+	}
+
+	void ContainerTest::terminate(SGE::Driver& driver)
+	{
+		delete m_uiDriver;
+		delete m_gfxDriver;
+	}
+
+	void ContainerTest::render(SGE::Driver& driver)
+	{
+		// SUTK::UIDriver::render() should only be called when there is an update in the UI or screen resize
+		m_uiDriver->render();
+	}
+
+	void ContainerTest::update(SGE::Driver& driver, float deltaTime)
+	{
+	}
+}
