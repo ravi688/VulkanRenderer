3d1b5f0a1295ded407b2f90b922178c77e6b11b2
ravi688 <rp0412204@gmail.com>
2024-10-10 07:34:13 +0530

    [SUTK] Move private fields into private: section

--- sutk/include/sutk/Scrollable.hpp
+++ sutk/include/sutk/Scrollable.hpp
@@ -9,8 +9,9 @@ namespace SUTK
 
 	class Scrollable
 	{
-	protected:
+	private:
 		ScrollContainer* m_scrollContainer;
+	protected:
 		ScrollContainer* getScrollContainer() noexcept { return m_scrollContainer; }
 		const ScrollContainer* getScrollContainer() const noexcept { return m_scrollContainer; }
 		Scrollable(Container* container) noexcept;
