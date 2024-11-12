1712972bd8443282a4d5d520b1bfe4b6457a4a24
ravi688 <rp0412204@gmail.com>
2024-11-12 19:11:59 +0530

    [SUTK] Only update draw orders for which isDrawOrderDirty() returns true

--- sutk/source/UIDriver.cpp
+++ sutk/source/UIDriver.cpp
@@ -112,7 +112,8 @@ namespace SUTK
 			for(auto it = m_renderables.begin(); it != m_renderables.end(); it++)
 			{
 				Renderable* renderable = (*it);
-				renderable->updateNormalizedDrawOrder((maxDrawOrder == minDrawOrder) ? 0.0f : ((1.0f - static_cast<f32>(renderable->getDrawOrder() - minDrawOrder) * normalizeFactor) * 99.0f));
+				if(renderable->isDrawOrderDirty())
+					renderable->updateNormalizedDrawOrder((maxDrawOrder == minDrawOrder) ? 0.0f : ((1.0f - static_cast<f32>(renderable->getDrawOrder() - minDrawOrder) * normalizeFactor) * 99.0f));
 			}
 		}
 
