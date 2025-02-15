2f01fc74d0902bad0a685d8738850289d328ba1e
ravi688 <rp0412204@gmail.com>
2025-02-16 00:42:54 +0530

    VSC: Migrated build system to build master

--- toolchain/shader_compiler/include/shader_compiler/defines.h
+++ toolchain/shader_compiler/include/shader_compiler/defines.h
@@ -26,16 +26,7 @@
 #pragma once
 
 #include <common/defines.h>
-
-#ifdef SC_STATIC_LIBRARY
-#	define SC_API
-#elif SC_DYNAMIC_LIBRARY
-#	define SC_API __declspec(dllimport)
-#elif BUILD_DYNAMIC_LIBRARY
-#	define SC_API __declspec(dllexport)
-#else
-#	define SC_API
-#endif
+#include <shader_compiler/api_defines.h>
 
 #include <stdlib.h>
 #include <string.h>
